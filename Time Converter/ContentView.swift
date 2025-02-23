a8e8f16a3fbceab49f07f592c8e42e35d1135652
Alex Jackson <ajjackson22@gmail.com>
2025-02-23 13:39:18 +0000

    Code for unit converter challenge day

--- Time Converter/ContentView.swift
+++ Time Converter/ContentView.swift
@@ -8,14 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var input: Double?
+    @State private var output: Double?
+    @State private var unitOne = "Minutes"
+    @State private var unitTwo = "Hours"
+    @FocusState private var inputIsFocused: Bool
+
+    let timeUnits = ["Hours", "Minutes", "Seconds", "Days"]
+
+    func convertToSeconds(input: Double) -> Double {
+        switch unitOne {
+        case "Hours":
+            return input * 3600
+        case "Minutes":
+            return input * 60
+        case "Seconds":
+            return input
+        case "Days":
+            return input * 86400
+        default:
+            return 0
         }
-        .padding()
+    }
+
+    var convertedValue: Double {
+        switch unitTwo {
+        case "Hours":
+            return convertToSeconds(input: input ?? 0) / 3600
+        case "Minutes":
+            return convertToSeconds(input: input ?? 0) / 60
+        case "Seconds":
+            return convertToSeconds(input: input ?? 0)
+        case "Days":
+            return convertToSeconds(input: input ?? 0) / 86400
+        default:
+            return 0
+        }
+    }
+
+    var body: some View {
+        NavigationStack {
+            
+            Form {
+                Section {
+                    TextField(
+                        "Time to convert", value: $input, format: .number
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($inputIsFocused)
+
+                    Picker("From", selection: $unitOne) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+
+                    Picker("To", selection: $unitTwo) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+
+                Section("Output") {
+                    Text(convertedValue, format: .number)
+                }
+            }
+            .navigationTitle("Time converter")
+            .toolbar {
+                if inputIsFocused {
+                    Button("Done") {
+                        inputIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
